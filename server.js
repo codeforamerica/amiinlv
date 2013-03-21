58000992b97b2fb1c436835161ba2e22b8a6b677
Ryan Closner <ryanc@codeforamerica.org>
2013-03-21 16:05:23 -0700

    Configure server to work with Heroku

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ var PORT = 3000
 
 var express   = require("express"),
     path      = require("path"),
-    http      = require("http"),
     locations = require("./routes/locations")
 
 var app = express()
 
 app.configure(function () {
   app.set( "port", process.env.PORT || PORT )
-  app.use( express.logger("dev") )
   app.use( express.bodyParser() )
   app.use( express.static( path.join( __dirname, "public") ) )
 })
@@ -17,6 +15,6 @@ app.configure(function () {
 app.get( "/geocode", locations.geocode )
 app.get( "/check",   locations.check )
 
-http.createServer( app ).listen( app.get("port"), function () {
+app.listen( app.get("port"), function () {
   console.log( "Listening on port " + app.get("port") )
 })
