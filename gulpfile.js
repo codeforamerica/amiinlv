89dae805c81a40d8b263ed5e712923a6eac3f1b3
Pieter Botha <pietboth@gmail.com>
2014-07-29 19:11:43 +0100

    Added gulp tasks for watching browserify bundle and rebuilding when any dependent modules change, likewise for less. Also added a single-go task that simply runs browserify on the bundle once and then quits. This is the default.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var gutil = require('gulp-util');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var mold = require('mold-source-map');
+var path = require('path');
+var browserify = require('browserify');
+
 
 gulp.task('less', function() {
     gulp.src('public/less/application.less')
@@ -9,3 +15,38 @@ gulp.task('less', function() {
         .on('error', gutil.log);
 });
 
+gulp.task('watch-js', function () {
+    var bundler = watchify('./src/application.js');
+
+    bundler.on('update', rebundleScripts);
+
+    function rebundleScripts() {
+        return bundler.bundle({debug: true})
+            .pipe(mold.transformSourcesRelativeTo(path.join(__dirname, 'public/js')))
+            .pipe(source('application.js'))
+            .pipe(gulp.dest('public/js'));
+    }
+
+    return rebundleScripts();
+});
+
+gulp.task('bundle-js-single', function() {
+    var bundler = browserify({entries:['./src/application.js'], debug: true});
+
+    return bundler.bundle()
+        .pipe(mold.transformSourcesRelativeTo(path.join(__dirname, 'public/js')))
+        .pipe(source('application.js'))
+        .pipe(gulp.dest('public/js'))
+        .on('end', gutil.log);
+});
+
+gulp.task('watch-css', function() {
+    gulp.watch('public/less/**/*.less', ['less']);
+});
+
+/* -- Tasks intended to be run for ease of use */
+/* usage: gulp */
+gulp.task('default', ['less', 'bundle-js-single']);
+
+/*usage: gulp watch */
+gulp.task('watch', ['watch-css', 'watch-js']);
