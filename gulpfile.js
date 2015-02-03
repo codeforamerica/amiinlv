1ba9a947c272acab0d19e31e95e6509a9fae9d50
Lou Huang <lou@louhuang.com>
2015-02-03 18:57:56 -0500

    Bump dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,52 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var gutil = require('gulp-util');
-var watchify = require('watchify');
-var source = require('vinyl-source-stream');
-var mold = require('mold-source-map');
-var path = require('path');
-var browserify = require('browserify');
+var gulp = require('gulp')
+var less = require('gulp-less')
+var gutil = require('gulp-util')
+var watchify = require('watchify')
+var source = require('vinyl-source-stream')
+var mold = require('mold-source-map')
+var path = require('path')
+var browserify = require('browserify')
 
-
-gulp.task('less', function() {
+gulp.task('less', function () {
   gulp.src('src/styles/application.less')
     .pipe(less())
     .pipe(gulp.dest('public/css/'))
-    .on('error', gutil.log);
-});
+    .on('error', gutil.log)
+})
 
 gulp.task('watch-js', function () {
-  var bundler = watchify('./src/application.js');
+  var bundler = watchify('./src/application.js')
 
-  bundler.on('update', rebundleScripts);
+  bundler.on('update', rebundleScripts)
 
-  function rebundleScripts() {
+  function rebundleScripts () {
     return bundler.bundle({debug: true})
       .pipe(mold.transformSourcesRelativeTo(path.join(__dirname, 'public/js')))
       .pipe(source('application.js'))
       .pipe(gulp.dest('public/js'))
-      .on('end', gutil.log);
+      .on('end', gutil.log)
   }
 
-  return rebundleScripts();
-});
+  return rebundleScripts()
+})
 
-gulp.task('bundle-js-single', function() {
-  var bundler = browserify({entries:['./src/application.js'], debug: true});
+gulp.task('bundle-js-single', function () {
+  var bundler = browserify({entries:['./src/application.js'], debug: true})
 
   return bundler.bundle()
     .pipe(mold.transformSourcesRelativeTo(path.join(__dirname, 'public/js')))
     .pipe(source('application.js'))
     .pipe(gulp.dest('public/js'))
-    .on('end', gutil.log);
-});
+    .on('end', gutil.log)
+})
 
-gulp.task('watch-css', function() {
-  gulp.watch('src/styles/**/*.less', ['less']);
-});
+gulp.task('watch-css', function () {
+  gulp.watch('src/styles/**/*.less', ['less'])
+})
 
 /* -- Tasks intended to be run for ease of use */
 /* usage: gulp */
-gulp.task('default', ['less', 'bundle-js-single']);
+gulp.task('default', ['less', 'bundle-js-single'])
 
 /*usage: gulp watch */
-gulp.task('watch', ['watch-css', 'watch-js']);
+gulp.task('watch', ['watch-css', 'watch-js'])
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-var PORT    = 3000,
-    express = require("express"),
-    path    = require("path"),
-    app     = express()
+var PORT = 3000
+var express = require('express')
+var path = require('path')
+var app = express()
 
+app.set('port', process.env.PORT || PORT)
+app.use(express.static('public'))
 
-app.configure(function () {
-  app.set( "port", process.env.PORT || PORT )
-  app.use( express.static( path.join( __dirname, "public") ) )
-})
-
-app.listen( app.get("port"), function () {
-  console.log( "Listening on port " + app.get("port") )
+app.listen(app.get('port'), function () {
+  console.log( 'Listening on port ' + app.get('port') )
 })
