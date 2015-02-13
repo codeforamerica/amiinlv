0f83b8b612f8bb381cb50cef3452202dde5e6aa6
Lou Huang <lou@louhuang.com>
2015-02-13 00:11:02 -0500

    Wrap initial geolocate in setTimeout to fix a Safari bug, closing #38. Remove unused PNG

--- src/current_location.js
+++ src/current_location.js
@@ -6,7 +6,11 @@ var getCurrentLocation = function (success, error) {
   }
 
   if (geolocator) {
-    geolocator.getCurrentPosition(success, error, options)
+    // Fixes an infinite loop bug with Safari
+    // https://stackoverflow.com/questions/27150465/geolocation-api-in-safari-8-and-7-1-keeps-asking-permission/28436277#28436277
+    window.setTimeout(function () {
+      geolocator.getCurrentPosition(success, error, options)
+    }, 0)
   } else {
     console.log('Browser does not support geolocation')
   }
